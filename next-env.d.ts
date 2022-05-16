8ffb3e250cd38145551f493a7ed4923d03015072
Brandon <brandon@cupocode.com>
2022-05-16 10:33:54 -0400

    changes pulled from remote

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
-=======
-/// <reference types="next" />
-/// <reference types="next/types/global" />
-/// <reference types="next/image-types/global" />
-
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
->>>>>>> 5916853dcfb47c3699343b26ef5debfe06a3fc8e
--- src/components/dashboard/total-profit.js
+++ src/components/dashboard/total-profit.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Avatar, Card, CardContent, Grid, Typography } from '@mui/material';
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 
@@ -40,46 +39,3 @@ export const TotalProfit = (props) => (
     </CardContent>
   </Card>
 );
-=======
-import { Avatar, Card, CardContent, Grid, Typography } from '@mui/material';
-import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
-
-export const TotalProfit = (props) => (
-  <Card {...props}>
-    <CardContent>
-      <Grid
-        container
-        spacing={3}
-        sx={{ justifyContent: 'space-between' }}
-      >
-        <Grid item>
-          <Typography
-            color="textSecondary"
-            gutterBottom
-            variant="overline"
-          >
-             MONTHLY PROFIT
-          </Typography>
-          <Typography
-            color="textPrimary"
-            variant="h4"
-          >
-            $0.00
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Avatar
-            sx={{
-              backgroundColor: 'primary.main',
-              height: 56,
-              width: 56
-            }}
-          >
-            <AttachMoneyIcon />
-          </Avatar>
-        </Grid>
-      </Grid>
-    </CardContent>
-  </Card>
-);
->>>>>>> 5916853dcfb47c3699343b26ef5debfe06a3fc8e
